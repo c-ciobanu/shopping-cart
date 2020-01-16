431361ed49e83fbf54c0aea6837a39e133764fd7
Cristi Ciobanu <c-ciobanu@protonmail.com>
2020-01-16 21:55:49 +0000

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Router } from "@reach/router";
+
+import Home from "containers/Home";
+import NotFound from "containers/NotFound";
 
 function App() {
-	return <div className="App">WIP</div>;
+	return (
+		<Router>
+			<Home path="/" />
+			<NotFound default />
+		</Router>
+	);
 }
 
 export default App;
