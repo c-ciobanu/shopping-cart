eb5a983cdb920382880374d821054b908b587e62
Cristi Ciobanu <c-ciobanu@protonmail.com>
2020-01-17 23:08:25 +0000

    add list page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Router } from "@reach/router";
 
 import Home from "containers/Home";
+import List from "containers/List";
 import NotFound from "containers/NotFound";
 
 function App() {
 	return (
 		<Router>
 			<Home path="/" />
+			<List path="lists/:listId" />
 			<NotFound default />
 		</Router>
 	);
