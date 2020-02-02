c628fedf85c20cc9c704f6edbddefe0a18cc171a
Cristi Ciobanu <c-ciobanu@protonmail.com>
2020-02-02 13:03:34 +0000

    update app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import { Router } from "@reach/router";
+import { createGlobalStyle } from "styled-components";
 
 import Home from "containers/Home";
 import List from "containers/List";
 import NotFound from "containers/NotFound";
+import Navigation from "containers/Navigation";
 
 import "sanitize.css";
 import "sanitize.css/forms.css";
 import "sanitize.css/typography.css";
 
+const GlobalStyle = createGlobalStyle`
+  main {
+		width: 90%;
+		margin: 0 auto;
+		padding-top: 68px;
+	}
+`;
+
 function App() {
 	return (
-		<Router>
-			<Home path="/" />
-			<List path="lists/:listId" />
-			<NotFound default />
-		</Router>
+		<>
+			<Navigation />
+
+			<main>
+				<Router>
+					<Home path="/" />
+					<List path="lists/:listId" />
+					<NotFound default />
+				</Router>
+			</main>
+
+			<GlobalStyle />
+		</>
 	);
 }
 
