d1f2994e862c3b2cca2ce4cf3ab28e69e550b671
Cristi Ciobanu <c-ciobanu@protonmail.com>
2020-02-15 18:57:51 +0000

    use Items in list page

--- src/containers/List/index.jsx
+++ src/containers/List/index.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 
 import FormInput from "components/FormInput";
+import Items from "components/Items";
 
 function List() {
 	const [products, setProducts] = useState([]);
 
 	const handleSubmit = (value) => {
-		setProducts(products.concat(value));
+		setProducts(products.concat({ name: value, bought: false }));
 	};
 
 	return (
 		<>
 			<FormInput onSubmit={handleSubmit} placeholder="Add item" />
 
-			{products.map((product) => (
-				<p>{product}</p>
-			))}
+			<Items options={products} onChange={() => {}} onDelete={() => {}} />
 		</>
 	);
 }
