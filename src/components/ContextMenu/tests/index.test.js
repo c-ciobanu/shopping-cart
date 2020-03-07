96ebcb0dbc696f359f011a1e2a5b3ca509d3c467
Cristi Ciobanu <c-ciobanu@protonmail.com>
2020-03-07 18:47:23 +0000

    fix eslint warnings

--- src/components/ContextMenu/tests/index.test.js
+++ src/components/ContextMenu/tests/index.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, getAllByRole } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import ContextMenu from "../index";
 
