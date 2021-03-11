38d380566e2a75eca2d09ca32d48a70a09ae777e
Cristi Ciobanu <c-ciobanu@protonmail.com>
2021-03-11 21:42:50 +0100

    use absolute imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import List from "./pages/List";
+import Home from "pages/Home";
+import List from "pages/List";
 
 export default function App(): JSX.Element {
 	return (
