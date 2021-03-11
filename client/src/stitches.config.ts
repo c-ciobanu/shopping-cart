1e58747316b5b667a14a2feaecf0e73a5aa862a1
Cristi Ciobanu <c-ciobanu@protonmail.com>
2021-03-11 21:42:25 +0100

    add stitches

---
+++ client/src/stitches.config.ts
@@ -0,0 +1,19 @@
+import { createStyled } from "@stitches/react";
+
+export const { styled, css } = createStyled({
+	prefix: "",
+	tokens: {
+		colors: {},
+		fonts: {},
+		fontSizes: {},
+		fontWeights: {},
+		lineHeights: {},
+		letterSpacings: {},
+		radii: {},
+		sizes: {},
+		space: {},
+		zIndices: {}
+	},
+	breakpoints: {},
+	utils: {}
+});
