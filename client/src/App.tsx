fd8a5bc562db2d28669f41fd95ed0dadb25dfeec
Cristi Ciobanu <c-ciobanu@protonmail.com>
2021-03-11 21:42:09 +0100

    add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import List from "./pages/List";
+
 export default function App(): JSX.Element {
 	return (
-		<div>
-			<p>WIP</p>
-		</div>
+		<Router>
+			<Switch>
+				<Route path="/">
+					<Home />
+				</Route>
+
+				<Route path="/lists/:id">
+					<List />
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
