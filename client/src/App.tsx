ad71419ce69ac6f9ba5b02607b58b35ddf25ea2d
Cristi Ciobanu <c-ciobanu@protonmail.com>
2021-03-14 12:45:17 +0100

    add navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,35 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { styled } from "stitches.config";
 import Home from "pages/Home";
 import List from "pages/List";
+import NavBar from "components/NavBar";
 
 import "sanitize.css";
 import "sanitize.css/typography.css";
 import "sanitize.css/forms.css";
 
+const StyledMain = styled("main", {
+	width: "90%",
+	margin: "0 auto",
+	paddingTop: "68px"
+});
+
 export default function App(): JSX.Element {
 	return (
 		<Router>
-			<Switch>
-				<Route path="/">
-					<Home />
-				</Route>
+			<NavBar />
 
-				<Route path="/lists/:id">
-					<List />
-				</Route>
-			</Switch>
+			<StyledMain>
+				<Switch>
+					<Route path="/lists/:id">
+						<List />
+					</Route>
+
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</StyledMain>
 		</Router>
 	);
 }
