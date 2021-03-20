bd553e2097db58e0246bd05b8b689d952ac3b11a
Cristi <c-ciobanu@protonmail.com>
2021-03-20 22:33:58 +0100

    add mocks of ResizeObserver and DOMRect

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+// @ts-ignore FIXME:
+global.ResizeObserver = class {
+	disconnect = jest.fn();
+	observe = jest.fn();
+	unobserve = jest.fn();
+};
+
+// @ts-ignore FIXME:
+global.DOMRect = {
+	fromRect: jest.fn()
+};
