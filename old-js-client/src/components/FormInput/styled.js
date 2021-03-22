a49d31f4a578c3d7bc973bd9abafe0ecf86145b5
Cristi <c-ciobanu@protonmail.com>
2021-03-22 23:24:05 +0100

    remove old-js-client folder

--- old-js-client/src/components/FormInput/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledInput = styled.input`
-	width: 100%;
-	padding: 10px;
-	border-radius: 3px;
-	border: 1px solid rgba(0, 0, 0, 0.12);
-	color: rgba(0, 0, 0, 0.87);
-`;
