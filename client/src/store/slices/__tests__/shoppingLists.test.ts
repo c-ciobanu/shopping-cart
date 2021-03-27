4878fd5c81a75dffb375b6aa519c30d7f522be55
Cristi <c-ciobanu@protonmail.com>
2021-03-27 12:17:13 +0100

    update shoppingLists reducer to handle items

--- client/src/store/slices/__tests__/shoppingLists.test.ts
+++ client/src/store/slices/__tests__/shoppingLists.test.ts
@@ -1,17 +1,83 @@
 import { AnyAction } from "@reduxjs/toolkit";
 
-import reducer, { addShoppingList, removeShoppingList } from "store/slices/shoppingLists";
+import reducer, {
+	addShoppingList,
+	removeShoppingList,
+	selectShoppingList,
+	addShoppingListItem,
+	removeShoppingListItem
+} from "store/slices/shoppingLists";
 
 describe("reducer", () => {
-	it("should handle initial state", () => {
+	it("should handle initial state correctly", () => {
 		expect(reducer(undefined, {} as AnyAction)).toEqual([]);
 	});
 
-	it("should handle addShoppingList correctly", () => {
-		expect(reducer(undefined, { type: addShoppingList.type, payload: "List 1" })).toEqual([{ id: 1, name: "List 1" }]);
-	});
+	describe("actions", () => {
+		it("should handle addShoppingList correctly", () => {
+			expect(reducer(undefined, { type: addShoppingList.type, payload: "List 1" })).toEqual([
+				{ id: 1, name: "List 1", items: [] }
+			]);
+		});
 
-	it("should handle removeShoppingList correctly", () => {
-		expect(reducer([{ id: 1, name: "List 1" }], { type: removeShoppingList.type, payload: 1 })).toEqual([]);
+		it("should handle removeShoppingList correctly", () => {
+			expect(reducer([{ id: 1, name: "List 1", items: [] }], { type: removeShoppingList.type, payload: 1 })).toEqual(
+				[]
+			);
+		});
+
+		describe("addShoppingListItem", () => {
+			it("should not add shopping list item if the shopping list does not exist", () => {
+				expect(
+					reducer([{ id: 1, name: "List 1", items: [] }], {
+						type: addShoppingListItem.type,
+						payload: { shoppingListId: 2, itemName: "List Item 1" }
+					})
+				).toEqual([{ id: 1, name: "List 1", items: [] }]);
+			});
+
+			it("should add shopping list item correctly", () => {
+				expect(
+					reducer([{ id: 1, name: "List 1", items: [] }], {
+						type: addShoppingListItem.type,
+						payload: { shoppingListId: 1, itemName: "List Item 1" }
+					})
+				).toEqual([{ id: 1, name: "List 1", items: [{ id: 1, name: "List Item 1" }] }]);
+			});
+		});
+
+		describe("removeShoppingListItem", () => {
+			it("should not remove shopping list item if the shopping list does not exist", () => {
+				expect(
+					reducer([{ id: 1, name: "List 1", items: [{ id: 1, name: "List Item 1" }] }], {
+						type: removeShoppingListItem.type,
+						payload: { shoppingListId: 2, itemId: 1 }
+					})
+				).toEqual([{ id: 1, name: "List 1", items: [{ id: 1, name: "List Item 1" }] }]);
+			});
+
+			it("should remove shopping list item correctly", () => {
+				expect(
+					reducer([{ id: 1, name: "List 1", items: [{ id: 1, name: "List Item 1" }] }], {
+						type: removeShoppingListItem.type,
+						payload: { shoppingListId: 1, itemId: 1 }
+					})
+				).toEqual([{ id: 1, name: "List 1", items: [] }]);
+			});
+		});
+	});
+});
+
+describe("selectors", () => {
+	describe("selectShoppingList", () => {
+		it("should return undefined if no shopping list is found", () => {
+			expect(selectShoppingList({ shoppingLists: [] })(1)).toEqual(undefined);
+		});
+
+		it("should return the matching shopping list", () => {
+			const shoppingList = { id: 1, name: "Shopping list 1", items: [] };
+
+			expect(selectShoppingList({ shoppingLists: [shoppingList] })(1)).toEqual(shoppingList);
+		});
 	});
 });
--- client/src/store/slices/shoppingLists.ts
+++ client/src/store/slices/shoppingLists.ts
@@ -1,8 +1,14 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 import { storeState } from "store";
 
 type ShoppingList = {
+	id: number;
+	name: string;
+	items: Array<ShoppingListItem>;
+};
+
+type ShoppingListItem = {
 	id: number;
 	name: string;
 };
@@ -14,18 +20,47 @@ const { actions, reducer } = createSlice({
 	initialState,
 	reducers: {
 		addShoppingList(state, action: PayloadAction<string>) {
-			const id = (state[state.length - 1]?.id ?? 0) + 1;
+			const newShoppingListId = (state[state.length - 1]?.id ?? 0) + 1;
 
-			return state.concat({ id, name: action.payload });
+			return state.concat({ id: newShoppingListId, name: action.payload, items: [] });
 		},
 		removeShoppingList(state, action: PayloadAction<number>) {
 			return state.filter((shoppingList) => shoppingList.id !== action.payload);
+		},
+		addShoppingListItem(state, action: PayloadAction<{ shoppingListId: number; itemName: string }>) {
+			const { shoppingListId, itemName } = action.payload;
+
+			const shoppingList = state.find((shoppingList) => shoppingList.id === shoppingListId);
+
+			if (!shoppingList) {
+				return state;
+			}
+
+			const newItemId = (shoppingList.items[shoppingList.items.length - 1]?.id ?? 0) + 1;
+
+			shoppingList.items.push({ id: newItemId, name: itemName });
+		},
+		removeShoppingListItem(state, action: PayloadAction<{ shoppingListId: number; itemId: number }>) {
+			const { shoppingListId, itemId } = action.payload;
+
+			const shoppingList = state.find((shoppingList) => shoppingList.id === shoppingListId);
+
+			if (!shoppingList) {
+				return state;
+			}
+
+			shoppingList.items = shoppingList.items.filter((item) => item.id !== itemId);
 		}
 	}
 });
 
-export const { addShoppingList, removeShoppingList } = actions;
-
 export const selectShoppingLists = (state: storeState): Array<ShoppingList> => state.shoppingLists;
+
+export const selectShoppingList = createSelector(selectShoppingLists, (shoppingLists) => {
+	return (shoppingListId: number): ShoppingList | undefined =>
+		shoppingLists.find((shoppingList) => shoppingList.id === shoppingListId);
+});
+
+export const { addShoppingList, removeShoppingList, addShoppingListItem, removeShoppingListItem } = actions;
 
 export default reducer;
