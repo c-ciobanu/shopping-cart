1a99910f76adcd4afc990ec03aa40b0ade00768f
Cristi <c-ciobanu@protonmail.com>
2021-04-19 21:56:03 +0200

    add createShoppingList test

--- backend/src/functions/__tests__/createShoppingList.test.js
+++ backend/src/functions/__tests__/createShoppingList.test.js
@@ -1,7 +1,26 @@
 import { ShoppingList } from "db/index";
-import { handler } from "functions/createShoppingList";
+import createShoppingList, { handler } from "functions/createShoppingList";
 
 describe("createShoppingList", () => {
+	it("should create a new list and return it", async () => {
+		const response = await createShoppingList({
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ name: "Breakfast" })
+		});
+
+		const responseBody = JSON.parse(response.body);
+
+		const newLists = await ShoppingList.findAll();
+
+		expect(responseBody).toEqual({
+			...newLists[0].toJSON(),
+			createdAt: expect.any(String),
+			updatedAt: expect.any(String)
+		});
+	});
+});
+
+describe("handler", () => {
 	it("should successfully return 200", async () => {
 		const response = await handler({ body: { name: "Breakfast" } });
 
