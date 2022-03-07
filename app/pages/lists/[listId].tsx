270bd0ce845e27131179a2d1376f73fd52347590
Cristi <c-ciobanu@protonmail.com>
2022-03-07 19:48:43 +0000

    sort items to split them in 2 sections based on the checked property

--- app/pages/lists/[listId].tsx
+++ app/pages/lists/[listId].tsx
@@ -18,17 +18,19 @@ import { AddItemFormDialog } from "app/items/components/AddItemFormDialog"
 import updateItem from "app/items/mutations/updateItem"
 import getItems from "app/items/queries/getItems"
 import { updateObjectInArray } from "app/core/utils/array"
+import sortBy from "lodash.sortby"
 
 const ShowListPage: BlitzPage = () => {
   const listId = useParam("listId", "number")
   const [list] = useQuery(getList, { id: listId })
   const [items, { setQueryData }] = useQuery(getItems, {
     where: { listId },
-    orderBy: { name: "asc" },
   })
   const [logoutMutation] = useMutation(logout)
   const [deleteItemMutation] = useMutation(deleteItem)
   const [updateItemMutation] = useMutation(updateItem)
+
+  const sortedItems = sortBy(items, ["checked", "name"])
 
   return (
     <>
@@ -61,8 +63,8 @@ const ShowListPage: BlitzPage = () => {
       </AppBar>
 
       <Stack spacing={2}>
-        {items.map((item, index) => (
-          <Card key={item.id} variant="outlined">
+        {sortedItems.map((item) => (
+          <Card key={item.id} variant={item.checked ? "elevation" : "outlined"}>
             <Stack direction="row" alignItems="center">
               <Checkbox
                 defaultChecked={item.checked}
@@ -72,7 +74,14 @@ const ShowListPage: BlitzPage = () => {
                     checked: !item.checked,
                   })
 
-                  setQueryData(updateObjectInArray(items, index, updatedItem), { refetch: false })
+                  setQueryData(
+                    updateObjectInArray(
+                      items,
+                      items.findIndex((i) => i.id === item.id),
+                      updatedItem
+                    ),
+                    { refetch: false }
+                  )
                 }}
               />
 
